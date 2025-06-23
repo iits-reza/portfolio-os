927faa165cd74747abc3abe113445351d19f2f86
rezamohdev <mohammadireza29@gmail.com>
2025-06-23 19:03:45 -0400

    ripple

--- src/components/NavigationRail/NavigationRail.jsx
+++ src/components/NavigationRail/NavigationRail.jsx
@@ -1,6 +1,7 @@
 import "@material/web/button/filled-tonal-button.js";
 import "@material/web/button/text-button.js";
-import React from "react";
+import { MdRipple } from "@material/web/ripple/ripple.js";
+import React, { useEffect, useRef } from "react";
 
 const navItems = [
   { label: "Projects", icon: "interests" },
@@ -11,55 +12,72 @@ const navItems = [
 function NavigationRail() {
   // const [isSelected, setIsSelected] = React.useState(false);
   const [selectedLabel, setSelectedLabel] = React.useState(null);
+  const rippleRefs = useRef([]);
+
+  useEffect(() => {
+    rippleRefs.current.forEach((ref) => {
+      if (ref) new MdRipple(ref);
+    });
+  }, []);
+
   return (
     <nav className="flex flex-col fixed top-0 left-0 bg-[#F7F2FA] h-full p-4 ">
-      <md-filled-tonal-button
-        style={{
-          backgroundColor: "#6e5586", // Your purple color or M3 token
-          borderRadius: "12px",
-          padding: 10,
-          width: 60,
-          height: 60,
-          display: "flex",
-          flex: "column",
-          justifyContent: "center",
-          alignContent: "center",
-        }}
-      >
-        <span className="material-symbols-outlined">home</span>
-      </md-filled-tonal-button>
-      {/* // todo : button anitmation */}
-      <div className="flex flex-col align-middle gap-5 mt-8">
-        {navItems.map((item) => {
-          const isSelected = selectedLabel === item.label;
-          return (
-            <div key={item.label} className="flex flex-col items-center gap-1">
-              <button
+      <div>
+        <md-filled-tonal-button
+          style={{
+            backgroundColor: "#6e5586", // Your purple color or M3 token
+            borderRadius: "12px",
+            padding: 10,
+            width: 60,
+            height: 60,
+            display: "flex",
+            flex: "column",
+            justifyContent: "center",
+            alignContent: "center",
+          }}
+        >
+          <span className="material-symbols-outlined">home</span>
+        </md-filled-tonal-button>
+        {/* // todo : button anitmation */}
+        <div className="flex flex-col align-middle gap-5 mt-8">
+          {navItems.map((item, index) => {
+            const isSelected = selectedLabel === item.label;
+            return (
+              <div
                 key={item.label}
-                onClick={() => setSelectedLabel(isSelected ? null : item.label)}
-                className={` rounded-[16px] cursor-pointer group hover:bg-purple-100 w-[56px] h-[32px] flex items-center justify-center ${
-                  isSelected && "bg-purple-200"
-                }`}
+                className="flex flex-col items-center gap-1"
               >
-                <span
-                  style={{
-                    fontVariationSettings: `'FILL' ${
-                      isSelected ? 1 : 0
-                    }, 'wght' ${isSelected ? 700 : 700}, 'GRAD' 0, 'opsz' 48`,
-                  }}
-                  className={
-                    isSelected
-                      ? "material-symbols"
-                      : "material-symbols-outlined"
+                <button
+                  ref={(el) => (rippleRefs.current[index] = el)}
+                  key={item.label}
+                  onClick={() =>
+                    setSelectedLabel(isSelected ? null : item.label)
                   }
+                  className={` rounded-[16px] cursor-pointer group hover:bg-purple-100 w-[56px] h-[32px] flex items-center justify-center ${
+                    isSelected && "bg-purple-200"
+                  }`}
                 >
-                  {item.icon}
-                </span>
-              </button>
-              <span className="text-sm">{item.label}</span>
-            </div>
-          );
-        })}
+                  <span
+                    style={{
+                      fontVariationSettings: `'FILL' ${
+                        isSelected ? 1 : 0
+                      }, 'wght' ${isSelected ? 700 : 700}, 'GRAD' 0, 'opsz' 48`,
+                    }}
+                    className={
+                      isSelected
+                        ? "material-symbols"
+                        : "material-symbols-outlined"
+                    }
+                  >
+                    {item.icon}
+                  </span>
+                </button>
+
+                <span className="text-sm">{item.label}</span>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </nav>
   );
