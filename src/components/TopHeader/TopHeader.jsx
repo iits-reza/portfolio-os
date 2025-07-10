5e611ee08ddfe0e5c8d636a44b129d5dd8c9756d
rezamohdev <mohammadireza29@gmail.com>
2025-07-09 20:12:11 -0400

    new shapes & animations for header

--- src/components/TopHeader/TopHeader.jsx
+++ src/components/TopHeader/TopHeader.jsx
@@ -3,64 +3,81 @@ import "./TopHeader.css";
 
 const shapes = [
   "arch.svg",
+  "puffyflowers.svg",
+  "diamond.svg",
   "sunny.svg",
   "bun.svg",
+  "puffydiamond.svg",
+  "flower.svg",
   "pill.svg",
-  "circle.svg",
+  "diamond.svg",
+  "heart.svg",
+  "puffydiamond.svg",
   "tewlvesidedcookie.svg",
+  "circle.svg",
+  "square.svg",
+  "tewlvesidedcookie.svg",
+  "heart.svg",
+  "pill.svg",
+  "triangle.svg",
+  "star.svg",
   "diamond.svg",
   "oval.svg",
+  "triangle.svg",
+  "tewlvesidedcookie.svg",
   "pixel-circle.svg",
   "square.svg",
+  "tewlvesidedcookie.svg",
   "seven-sided-cookie.svg",
 ];
-function getRandomColor() {
-  const colors = [
-    "#F87171", // red
-    "#FBBF24", // yellow
-    "#34D399", // green
-    "#60A5FA", // blue
-    "#A78BFA", // purple
-    "#F472B6", // pink
-    "#FCD34D", // amber
-    "#818CF8", // indigo
-    "#2DD4BF", // teal
-    "#FB923C", // orange
-  ];
-  return colors[Math.floor(Math.random() * colors.length)];
-}
 
 function getRandomPosition() {
-  const top = Math.floor(Math.random() * 90); // % of parent height
-  const left = Math.floor(Math.random() * 90); // % of parent width
+  const top = Math.floor(Math.random() * 90);
+  const left = Math.floor(Math.random() * 90);
   return { top: `${top}%`, left: `${left}%` };
 }
 
+const animations = [
+  "animate-spin",
+  "animate-ping",
+  "animate-pulse",
+  "animate-bounce",
+];
 function TopHeader() {
   const [positions, setPositions] = useState([]);
+  const [animation, setAnimation] = useState("");
 
   useEffect(() => {
     const randomPositions = shapes.map(() => getRandomPosition());
     setPositions(randomPositions);
+    const changeAnimation = () => {
+      const random = animations[Math.floor(Math.random() * animations.length)];
+      setAnimation(random);
+    };
+    changeAnimation();
+    const interval = setInterval(changeAnimation, 5000);
+    return () => clearInterval(interval); // cleanup
   }, []);
 
   return (
-    <header className="relative w-full h-screen overflow-hidden flex flex-col justify-center">
+    <header className="relative w-full h-screen overflow-hidden flex flex-col justify-center ">
       {shapes.map((shape, idx) => (
         <img
           key={idx}
           src={`./shapes/${shape}`}
           alt={`shape-${idx}`}
-          className="h-24 w-24 absolute float"
+          className={`absolute  ${animation}`}
           style={{
             top: positions[idx]?.top,
             left: positions[idx]?.left,
-            animation: "spin 20s linear infinite",
+            width: Math.random() * 120,
+            animationDuration: "3s",
           }}
         />
       ))}
-      <h1 className="text-7xl text-center z-10">
-        I'm Reza a Front end Developer
+      <h1 className="text-7xl text-center z-10 text-shadow-white text-shadow-2xs">
+        Hey there, I'm Reza
+        <div />A Front end Developer
       </h1>
     </header>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@material-design-icons/font";
 import "material-symbols";
+import "./main.css";
 
 import "./index.css";
 import App from "./App.jsx";
